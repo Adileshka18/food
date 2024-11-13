937f9c398f2c79bcfd3ce0b2ac99951fc9747614
Adile Ashirova <ashirova.a.i.23@gmail.com>
2024-11-13 08:58:47 +0300

    Пункт 4_2

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,17 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+/* Страница шашлыка*/
+router.get('/shashlik', function(req, res, next) {
+    res.send("<h1>Страница Шашлыка</h1>")
+    });
+    /* Страница суши*/
+router.get('/susi', function(req, res, next) {
+    res.send("<h1>Страница Суш</h1>")
+    });
+    /* Страница пицца*/
+router.get('/pizza', function(req, res, next) {
+    res.send("<h1>Страница Пиццы</h1>")
+    });
+    
 module.exports = router;
