a29f08fafab2f7f01d02123d3fb99f0d371b3767
Adile Ashirova <ashirova.a.i.23@gmail.com>
2024-11-17 18:51:17 +0300

    Пункт 6_2

---
+++ data.js
@@ -0,0 +1,20 @@
+var data = [{
+    title: 'Шашлык',
+    nick: 'shashlik',
+    avatar: '/images/shashlik.jpg',
+    desc: 'шашлык это вкусно.'
+    },
+    {
+    title: 'Суши',
+    nick: 'susi',
+    avatar: '/images/susi.jpg',
+    desc: 'суши еще лучше'
+    },
+    {
+    title: 'Пицца',
+    nick: 'pizza',
+    avatar: '/images/pizza.jpg',
+    desc: 'пицца кайф'
+    }
+    ];
+    module.exports.data = data;
