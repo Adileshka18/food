c164ba3cf90818713d578233d54e0d1e2b2867b7
Adile Ashirova <ashirova.a.i.23@gmail.com>
2024-11-20 13:19:51 +0300

    Task 7_2

--- testMongoose.js
+++ testMongoose.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-const Food = mongoose.model('Food', { name: String });
-const dish = new Food({ name: 'шашлык' });
-dish.save().then(() => console.log('нямням'));
+var schema = mongoose.Schema({ name: String })
+schema.methods.niam = function(){
+console.log(this.name + " очень вкусный")
+}
+const Food = mongoose.model('Food', schema);
+const dish = new Food({ name: 'Шашлык' });
+dish.save().then(() => dish.niam());
