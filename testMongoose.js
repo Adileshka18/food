de2703293627597430ecad8c07c652be30e639c7
Adile Ashirova <ashirova.a.i.23@gmail.com>
2024-11-21 08:35:26 +0300

    Task 7_3

--- testMongoose.js
+++ testMongoose.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-var schema = mongoose.Schema({ name: String })
-schema.methods.niam = function(){
-console.log(this.name + " очень вкусный")
-}
-const Food = mongoose.model('Food', schema);
-const dish = new Food({ name: 'Шашлык' });
-dish.save().then(() => dish.niam());
+ mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
+ var Food = require('./models/food.js').Food
+ var food  = new Food({
+ title: "Шашлык",
+ nick: "shashlik"
+ })
+ food.save();
