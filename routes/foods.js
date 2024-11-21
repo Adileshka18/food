e8198689a69b75f25c3cc03b4eedae93aaca6dd0
Adile Ashirova <ashirova.a.i.23@gmail.com>
2024-11-21 09:31:14 +0300

    Task 8_3

--- routes/foods.js
+++ routes/foods.js
@@ -1,15 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var Food = require('../models/food').Food;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с foods');
 });
 
-/* Страница шашлыков */
+/* Страница музыки */
 router.get("/:nick", function(req, res, next) {
     res.send(req.params.nick);
     });
-    
+
+    router.get("/:nick", async function(req, res, next) {
+        var foods = await food.find({nick: req.params.nick});
+        console.log(musics)
+        if(!musics.length) return next(new Error("Нет такого блюда"))
+        var food = foods[0];
+        res.render('food', {
+        title: food.title,
+        picture: food.avatar,
+        desc: food.desc
+        })
+        });    
 
 module.exports = router;
