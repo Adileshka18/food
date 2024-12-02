59a0faa87c7269739c70ee62b54e5d3c93609f42
Adile Ashirova <ashirova.a.i.23@gmail.com>
2024-12-02 13:38:20 +0300

    Task 10_8

--- routes/foods.js
+++ routes/foods.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var Food = require('../models/food').Food;
+var checkAuth = require("../middlewares/checkAuth.js");
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с foods');
 });
 
-router.get("/:nick", async function (req, res, next) {
+router.get("/:nick",checkAuth, async function (req, res, next) {
   var foods = await Food.find({ nick: req.params.nick });
   console.log(foods)
   if (!foods.length) return next(new Error("Нет такого блюда"))
